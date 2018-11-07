36862e9a3b4a170104199e647effa2e5bc45743e
rarias <riqarias@gmail.com>
2018-11-07 20:57:23 -0300

    herramientas

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 /** 
 * main.c
 **/
+// El proyecto se dearrolló con Atmel Studio 6 (Version: 6.2.1563 - Service Pack 2) © 2014 Atmel Corp. All rights reserved.
+// AVR Assembler Version: 2.1.57.16
+// Atmel AVR (8 bit) GNU Toolchain - 3.4.5.1061  GCC_VERSION:4.8.1
+
+#define F_CPU 8000000UL	// clock del micro en MHz
+#include <avr/io.h>     // definiciones de registros, bits y constantes del MCU
 
 void main(void){
 
